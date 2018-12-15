f9ea685d1c2ca254ddc2fc90ba019e8eb76954ab
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2018-12-15 17:00:24 +0100

    Fix warnings

--- D&DManager/src/menu/MyMenu.java
+++ D&DManager/src/menu/MyMenu.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import layout.*;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 public class MyMenu extends JMenuBar {
 
