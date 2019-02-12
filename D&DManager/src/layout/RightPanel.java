4a8ab6823e98d8729b3a19dd5d760490be34dcfc
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2019-02-12 18:29:47 +0100

    Add GraphicDice in the right layour #ToFix

--- D&DManager/src/layout/RightPanel.java
+++ D&DManager/src/layout/RightPanel.java
@@ -2,9 +2,17 @@ package layout;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import components.GraphicDice;
 
+/**
+ * 
+ * @author Yuri	
+ *	This class describes the right panel of the main layout of the application
+ */
 public class RightPanel extends CustomPanel {
 
 	/**
@@ -14,11 +22,21 @@ public class RightPanel extends CustomPanel {
 
 	public RightPanel(Dimension d, Color bg) {
 		super(d, bg);
+		this.setLayout(new FlowLayout());
+		
+		//generate 10 buttons for test purposes
 		
 		for(int i = 0; i < 10; i++) {
 			JButton btn = new JButton("test"+i);
 			this.add(btn);
 		}
+		
+		//add Graphic Dice  TODO: doesn't work
+		GraphicDice dice= new GraphicDice(4,50, new Dimension(50,50), Color.white);
+		
+		this.add(dice);
+	
+		System.out.println("Dice dimension: "+ dice.getSize());
 	}
 
 }
