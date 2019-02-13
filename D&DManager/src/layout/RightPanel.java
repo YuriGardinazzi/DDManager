fcc5bd1fed8b300c4a7c1f6bf5eeb04eab11b6b2
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2019-02-13 18:03:54 +0100

    add Constraint layout to the right panel

--- D&DManager/src/layout/RightPanel.java
+++ D&DManager/src/layout/RightPanel.java
@@ -3,7 +3,8 @@ package layout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import components.GraphicDice;
@@ -22,21 +23,24 @@ public class RightPanel extends CustomPanel {
 
 	public RightPanel(Dimension d, Color bg) {
 		super(d, bg);
-		this.setLayout(new FlowLayout());
-		
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		//generate 10 buttons for test purposes
-		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;	
+		c.gridy= 0;
 		for(int i = 0; i < 10; i++) {
 			JButton btn = new JButton("test"+i);
-			this.add(btn);
+			c.gridx++;
+			this.add(btn, c);
 		}
 		
 		//add Graphic Dice  TODO: doesn't work
-		GraphicDice dice= new GraphicDice(4,50, new Dimension(50,50), Color.white);
-		
-		this.add(dice);
+		//	GraphicDice dice= new GraphicDice(4,50, new Dimension(100,100), Color.white);
+		//c.gridy++;
+		//this.add(dice, c);
 	
-		System.out.println("Dice dimension: "+ dice.getSize());
+	//	System.out.println("Dice dimension: "+ dice.getSize());
 	}
 
 }
