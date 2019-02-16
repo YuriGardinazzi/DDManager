f73347edd93454fc5f322b2825aca95ef1f02a85
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2019-02-16 16:11:56 +0100

    Create character class

---
+++ D&DManager/src/tools/Character.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package tools;
+
+/**
+ * @author Yuri
+ * This class describes a general character
+ */
+public class Character {
+
+	private String name;
+	private String alignment;
+	private String divinity;
+	
+	private Integer strength;
+	private Integer	dexterity;
+	private Integer	costitution;
+	private Integer	intelligence;
+	private Integer	wisdom;
+	private Integer charisma;
+	
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the alignment
+	 */
+	public String getAlignment() {
+		return alignment;
+	}
+	/**
+	 * @param alignment the alignment to set
+	 */
+	public void setAlignment(String alignment) {
+		this.alignment = alignment;
+	}
+	/**
+	 * @return the divinity
+	 */
+	public String getDivinity() {
+		return divinity;
+	}
+	/**
+	 * @param divinity the divinity to set
+	 */
+	public void setDivinity(String divinity) {
+		this.divinity = divinity;
+	}
+	/**
+	 * @return the strength
+	 */
+	public Integer getStrength() {
+		return strength;
+	}
+	/**
+	 * @param strength the strength to set
+	 */
+	public void setStrength(Integer strength) {
+		this.strength = strength;
+	}
+	/**
+	 * @return the dexterity
+	 */
+	public Integer getDexterity() {
+		return dexterity;
+	}
+	/**
+	 * @param dexterity the dexterity to set
+	 */
+	public void setDexterity(Integer dexterity) {
+		this.dexterity = dexterity;
+	}
+	/**
+	 * @return the costitution
+	 */
+	public Integer getCostitution() {
+		return costitution;
+	}
+	/**
+	 * @param costitution the costitution to set
+	 */
+	public void setCostitution(Integer costitution) {
+		this.costitution = costitution;
+	}
+	/**
+	 * @return the intelligence
+	 */
+	public Integer getIntelligence() {
+		return intelligence;
+	}
+	/**
+	 * @param intelligence the intelligence to set
+	 */
+	public void setIntelligence(Integer intelligence) {
+		this.intelligence = intelligence;
+	}
+	/**
+	 * @return the wisdom
+	 */
+	public Integer getWisdom() {
+		return wisdom;
+	}
+	/**
+	 * @param wisdom the wisdom to set
+	 */
+	public void setWisdom(Integer wisdom) {
+		this.wisdom = wisdom;
+	}
+	/**
+	 * @return the charisma
+	 */
+	public Integer getCharisma() {
+		return charisma;
+	}
+	/**
+	 * @param charisma the charisma to set
+	 */
+	public void setCharisma(Integer charisma) {
+		this.charisma = charisma;
+	}
+}
