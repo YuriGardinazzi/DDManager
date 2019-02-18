d6a198363ab3b34887c5388bbcb343d36cc24b7d
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2019-02-18 14:13:45 +0100

    Add close feature after saving a character

--- D&DManager/src/layout/CharacterCreation.java
+++ D&DManager/src/layout/CharacterCreation.java
@@ -7,6 +7,7 @@ import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -319,6 +320,9 @@ public class CharacterCreation extends JFrame {
 			         fileIn.close();
 				
 			         System.out.println(c);
+			         
+			         //Close the character creation frame
+			         this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
 					
 		        } catch (Exception ex) {
 		        	JOptionPane.showMessageDialog(null, "Something went wrong :C");
