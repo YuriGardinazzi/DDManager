bd01363bb3f3d892aea0706b31166556b6459d80
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2019-02-20 16:17:14 +0100

    Fix JFormattedTextField

--- D&DManager/src/graphical_components/FormNumber.java
+++ D&DManager/src/graphical_components/FormNumber.java
@@ -4,7 +4,10 @@
 package graphical_components;
 
 import java.awt.Font;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 import javax.swing.JFormattedTextField;
 import javax.swing.text.NumberFormatter;
@@ -15,27 +18,36 @@ import javax.swing.text.NumberFormatter;
  * @author Yuri
  * 
  */
-public class FormNumber extends JFormattedTextField {
+public class FormNumber extends JFormattedTextField implements PropertyChangeListener{
 
     //TODO: Aggiungere controllo sull'input come si deve
 	private String font = "Times new roman";
 	private int fontSize = 18;
+	private NumberFormat nFormat;
 	public FormNumber() {
-		super();
-		
-		DecimalFormat decimalFormat = new DecimalFormat("###");
-		decimalFormat.setMinimumIntegerDigits(0);
-		
-		NumberFormatter formatter = new NumberFormatter(decimalFormat);
-		this.setFormatter(formatter);
+		super(NumberFormat.getNumberInstance());
+		this.setValue(0);
+		this.addPropertyChangeListener("value",this);
 		this.setFont(new Font(this.font, Font.PLAIN, this.fontSize));
 		this.setText("0");
 		
-	
-		
 	}
+
 	public Integer getNumber() {
 		return Integer.parseInt(this.getText());
+	}
+
+	/**
+	 * Checks for negative values and set them to 0
+	 */
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		Integer input = ((Number) this.getValue()).intValue();
+		
+		if(input < 0) {
+			this.setValue(0);
+		}
+		
 	}
 }
 
